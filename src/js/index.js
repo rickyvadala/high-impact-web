bed768a6fac6f83f9fb8791858614e19ff49bf20
Ricardo Vadala <rickyvad_94@hotmail.com>
2021-02-04 18:16:57 -0300

    Webpack

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+// The commented ones has trouble with webpack so they are in the index.html individually
+import '../index.html';
+
+//CSS
+import '../css/font-awesome-all.css'
+import '../css/flaticon.css'
+import '../css/owl.css'
+import '../css/bootstrap.css'
+import '../css/jquery.fancybox.min.css'
+import '../css/animate.css'
+import '../css/style.css'
+import '../css/responsive.css'
+import '../css/custom.css'
+
+//JS
+// import './jquery.js'
+// import './popper.min.js'
+// import './bootstrap.min.js'
+// import './owl.js'
+// import './wow.js'
+// import './jquery.fancybox.js'
+// import './appear.js'
+// import './scrollbar.js'
+// import './jquery.paroller.min.js'
+// import './tilt.jquery.js'
+// import './script.js'
+// import './custom.js'
